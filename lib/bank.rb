fbe4e3793c9563049ae0c470030fa5e435f6a45c
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-12-16 15:01:02 +0000

    included interpolated value for 'date' in make_a_deposit method.

--- lib/bank.rb
+++ lib/bank.rb
@@ -6,12 +6,13 @@ class Bank
 
   def initialize(balance = 0)
     @balance = balance
+    @today = DateTime.now.strftime("%d/%m/%Y")
   end
 
   def make_a_deposit(amount)
     @amount = amount
     @balance += @amount
     fail "Error: Your deposit must be above £#{MINIMUM_AMOUNT}.00" unless @amount > MINIMUM_AMOUNT
-    "You've deposited £#{@amount}.00 on 16/12/2019 and your balance is: £#{@balance}.00"
+    "You've deposited £#{@amount}.00 on #{@today} and your balance is: £#{@balance}.00"
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,9 +5,10 @@ describe Bank do
     let(:natwest) { described_class.new }
     let(:amount)  { amount = 1000 }
     let(:invalid_amount) { invalid_amount = 0 }
+    let(:today) { today = DateTime.now.strftime("%d/%m/%Y") }
 
     it "takes 1000 and returns You've deposited £1000.00 on 16/12/2019 and your balance is: £1000.00" do
-      expect(natwest.make_a_deposit(amount)).to eq "You've deposited £#{amount}.00 on 16/12/2019 and your balance is: £#{natwest.balance}.00"
+      expect(natwest.make_a_deposit(amount)).to eq "You've deposited £#{amount}.00 on #{today} and your balance is: £#{natwest.balance}.00"
     end
 
     it "takes 0 and returns 'Error: Your deposit must be above £1.00'" do
