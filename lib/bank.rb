5e6a6e5a42eedf55126ddc4c0623a2fde892c724
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-12-16 14:07:27 +0000

    - added interpolated deposit amount  - added make_a_deposit method  - added initialised 'balance' amount  - added guard clause against deposit amounts < 0  - added static date value for deposit confirmation message

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,5 +1,8 @@
+require 'date'
 
 class Bank
+  attr_reader :balance
+
   def initialize(balance = 0)
     @balance = balance
   end
@@ -7,6 +10,7 @@ class Bank
   def make_a_deposit(amount)
     @amount = amount
     @balance += @amount
-    @amount > 0 ? "You've deposited £#{@amount}.00 on 16/12/2012 and your balance is: £#{@balance}.00" : "Error: Your deposit must be above £0.00"
+    fail "Error: Your deposit must be above £0.00" unless @amount > 0
+    "You've deposited £#{@amount}.00 on 16/12/2019 and your balance is: £#{@balance}.00"
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,9 +5,12 @@ describe Bank do
     let(:natwest) { described_class.new }
     let(:amount)  { amount = 1000 }
 
-    it "takes 1000 and returns You've deposited £1000.00 on 16/12/2012 and your balance is: £1000.00" do
-      expect(natwest.make_a_deposit(amount)).to eq "You've deposited £#{amount}.00 on 16/12/2012 and your balance is: £1000.00"
+    it "takes 1000 and returns You've deposited £1000.00 on 16/12/2019 and your balance is: £1000.00" do
+      expect(natwest.make_a_deposit(amount)).to eq "You've deposited £#{amount}.00 on 16/12/2019 and your balance is: £#{natwest.balance}.00"
     end
 
+    it "takes 0 and returns 'Error: Your deposit must be above £0.00'" do
+      expect { natwest.make_a_deposit(0) }.to raise_error "Error: Your deposit must be above £0.00"
+    end
   end
 end
