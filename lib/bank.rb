c352b6ecefa93f4042ea6bbb3a09016b7039a496
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-12-16 14:35:03 +0000

    added test for minimum amount, and included interpolated value for MINIMUM_AMOUNT in make_a_deposit method guard clause.

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,6 +1,7 @@
 require 'date'
 
 class Bank
+  MINIMUM_AMOUNT = 1
   attr_reader :balance
 
   def initialize(balance = 0)
@@ -10,7 +11,7 @@ class Bank
   def make_a_deposit(amount)
     @amount = amount
     @balance += @amount
-    fail "Error: Your deposit must be above £0.00" unless @amount > 0
+    fail "Error: Your deposit must be above £#{MINIMUM_AMOUNT}.00" unless @amount > MINIMUM_AMOUNT
     "You've deposited £#{@amount}.00 on 16/12/2019 and your balance is: £#{@balance}.00"
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,13 +4,14 @@ describe Bank do
   context "Make deposit" do
     let(:natwest) { described_class.new }
     let(:amount)  { amount = 1000 }
+    let(:invalid_amount) { invalid_amount = 0 }
 
     it "takes 1000 and returns You've deposited £1000.00 on 16/12/2019 and your balance is: £1000.00" do
       expect(natwest.make_a_deposit(amount)).to eq "You've deposited £#{amount}.00 on 16/12/2019 and your balance is: £#{natwest.balance}.00"
     end
 
-    it "takes 0 and returns 'Error: Your deposit must be above £0.00'" do
-      expect { natwest.make_a_deposit(0) }.to raise_error "Error: Your deposit must be above £0.00"
+    it "takes 0 and returns 'Error: Your deposit must be above £1.00'" do
+      expect { natwest.make_a_deposit(0) }.to raise_error("Error: Your deposit must be above £#{Bank::MINIMUM_AMOUNT}.00")
     end
   end
 end
